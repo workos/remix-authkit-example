1824b59abf11bb85a7781acd812a97d0c9843418
Nick Nisi <nick.nisi@workos.com>
2025-01-15 16:19:24 -0600

    add v3 compatibility flags

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
       future: {
         unstable_optimizeDeps: true,
         v3_relativeSplatPath: true,
+        v3_fetcherPersist: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+        v3_throwAbortReason: true,
       },
     }),
   ],

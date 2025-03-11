269444bec5d8e6e45162380cb3aff8f6730e2c38
Nick Nisi <nick.nisi@workos.com>
2025-03-11 16:39:13 -0500

    update to to latest remix version / vite config

--- app/root.tsx
+++ app/root.tsx
@@ -1,51 +1,47 @@
 // Import the base CSS styles for the radix-ui components.
-import '@radix-ui/themes/styles.css';
-import { Theme, Card, Container, Flex, Button, Box } from '@radix-ui/themes';
+import "@radix-ui/themes/styles.css";
+import { Theme, Card, Container, Flex, Button, Box } from "@radix-ui/themes";
 
-import { cssBundleHref } from '@remix-run/css-bundle';
 import type {
   ActionFunctionArgs,
   LinksFunction,
   LoaderFunctionArgs,
-} from '@remix-run/node';
+} from "@remix-run/node";
+
 import {
   Links,
   Link,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
   useRouteLoaderData,
-  json,
-} from '@remix-run/react';
+} from "@remix-run/react";
 
-import Footer from './components/footer';
-import SignInButton from './components/sign-in-button';
+import Footer from "./components/footer";
+import SignInButton from "./components/sign-in-button";
 
 import {
   getSignInUrl,
   signOut,
   authkitLoader,
-} from '@workos-inc/authkit-remix';
+} from "@workos-inc/authkit-remix";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
-];
+export const links: LinksFunction = () => [];
 
 export const loader = (args: LoaderFunctionArgs) =>
   authkitLoader(
     args,
     async () => {
-      return json({
+      return {
         signInUrl: await getSignInUrl(),
-      });
+      };
     },
     { debug: true }
   );
 
 export function useRootLoaderData() {
-  return useRouteLoaderData<typeof loader>('root');
+  return useRouteLoaderData<typeof loader>("root");
 }
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -65,13 +61,12 @@ export default function App() {
       <body>
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
         <Theme
           accentColor="iris"
           panelBackground="solid"
-          style={{ backgroundColor: 'var(--gray-1)' }}
+          style={{ backgroundColor: "var(--gray-1)" }}
         >
-          <Container style={{ backgroundColor: 'var(--gray-1)' }}>
+          <Container style={{ backgroundColor: "var(--gray-1)" }}>
             <Flex direction="column" gap="5" p="5" height="100vh">
               <Box asChild flexGrow="1">
                 <Card size="4">
@@ -84,7 +79,7 @@ export default function App() {
                           </Button>
 
                           <Button asChild variant="soft">
-                            <Link to="/account">Account</Link>
+                            <Link to="./account">Account</Link>
                           </Button>
                         </Flex>
 
